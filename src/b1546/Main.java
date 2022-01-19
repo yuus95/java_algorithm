bab54db9417aff8af39b0dfdcc7f69ce4c40c212
yushin <kkad45@naver.com>
2022-01-19 22:20:21 +0900

    [add] 백준 1546번,2753번

---
+++ src/b1546/Main.java
@@ -0,0 +1,29 @@
+package b1546;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+            soludtion();
+    }
+    public static void soludtion(){
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        double[] scoreBox = new double[len];
+        double maxNum = -1;
+        int idx = 0;
+        for(int i = 0 ; i<len; i++){
+            scoreBox[i] = sc.nextInt();
+            if(maxNum < scoreBox[i]){
+                maxNum = scoreBox[i];
+                idx = i; 
+            }
+        }
+        double sum = 0;
+        for(int i =0; i<len;i++){
+            sum+= scoreBox[i]/  maxNum *100;
+        }
+        System.out.println(sum/len);
+    }
+
+}
