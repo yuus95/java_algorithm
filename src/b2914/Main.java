389674ff82f8eb8be2a7fa287d83336114d37a74
yushin <kkad45@naver.com>
2022-01-21 22:10:07 +0900

    [add] 2914번 성공

--- src/b2914/Main.java
+++ src/b2914/Main.java
@@ -1,8 +1,6 @@
 package b2914;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Main {
@@ -13,29 +11,53 @@ public class Main {
     public static void solution() {
         Scanner sc = new Scanner(System.in);
         String inp = sc.nextLine();
-        if (inp.length() == 0) {
-            System.out.println(0);
-        }
-        char[] inpArray = inp.toCharArray();
-        int cnt = 0;
-        List<String> stringList = new ArrayList<String>(Arrays.asList("c=","c-","dz=","d-","lj","nj","s=","z="));
+        int count = 0;
+        for (int i = 0; i < inp.length(); i++) {
+            if (inp.charAt(i) == 'c') {
+                if (i + 1 < inp.length()) {
+                    if (inp.charAt(i + 1) == '=' || inp.charAt(i + 1) == '-') {
+                        i++;
+                    }
+                }
+            } else if (inp.charAt(i) == 'd') {
+                if (i + 1 < inp.length()) {
+                    if (inp.charAt(i + 1) == 'z') {
+                        if ( i + 2 < inp.length() && inp.charAt(i + 2) == '=') {
+                            i=i+2;
 
-        int start = 0;
-        int idx = 1;
-        for(int i = 0; i<inp.length()-1;i++){
-            char temp = inp.charAt(i);
-            System.out.println( "----${temp}--- " + inp.charAt(i));
-
-            for(int j=1; j<3;j++){
-                if(i+j<inp.length()){
-                    System.out.println(temp);
-                    if (stringList.contains(temp)){
-                        System.out.println("---${temp}---");
+                        }
+                    }
+                    else if (inp.charAt(i + 1) == '-') {
+                        i++;
+                    }
+                }
+            } else if (inp.charAt(i) == 'l') {
+                if (i + 1< inp.length()) {
+                    if (inp.charAt(i + 1) == 'j') {
+                        i++;
+                    }
+                }
+            } else if (inp.charAt(i) == 'n') {
+                if (i + 1 < inp.length()) {
+                    if (inp.charAt(i + 1) == 'j') {
+                        i++;
+                    }
+                }
+            } else if (inp.charAt(i) == 's') {
+                if (i + 1< inp.length()) {
+                    if (inp.charAt(i + 1) == '=') {
+                        i++;
+                    }
+                }
+            } else if (inp.charAt(i) == 'z') {
+                if (i + 1< inp.length()) {
+                    if (inp.charAt(i + 1) == '=') {
+                        i++;
                     }
                 }
             }
+            count++;
         }
-
-
+        System.out.println(count);
     }
 }
