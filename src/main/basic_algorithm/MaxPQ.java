e0f4a4d3f01b512744d2c2161f8c89aad283c1dc
yushin <kkad45@naver.com>
2023-07-15 16:49:50 +0900

    알고리즘 스터디  이진힙을 이용한 우선순위 큐

---
+++ src/main/basic_algorithm/MaxPQ.java
@@ -0,0 +1,65 @@
+package basic_algorithm;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax() {
+        Key max = pq[1];
+        exch(1, N--);
+        pq[N + 1] = null;
+        sink(1);
+        return max;
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key temp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = temp;
+    }
+
+    private void swim(int k) {
+        // 상향식이므로 K는 1이되면 안된다. 아래에서 위로 올라갈 수 가 없다.
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) {
+                j++;
+            }
+
+            if (!less(k, j)) break;
+
+            exch(k, j);
+
+            k = j;
+        }
+
+    }
+}
